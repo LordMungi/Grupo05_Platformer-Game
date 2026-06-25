8e0a3ca9533ce922f4a63c3cb793eea7ed40e9b1
Nikkie-game-dev <nikkiegallardo@outlook.com.ar>
2026-06-25 13:13:20 -0300

    Add long jump mechanic

--- Assets/Assets/Code/Player/Controller.cs
+++ Assets/Assets/Code/Player/Controller.cs
@@ -4,15 +4,24 @@ using UnityEngine.InputSystem;
 
 namespace Assets.Code.Player
 {
+    public delegate void LongJumpStarted();
+
     [RequireComponent(typeof(Rigidbody2D))]
     public class Controller : MonoBehaviour
     {
+        public event LongJumpStarted OnLongJumpStarted;
+
         [SerializeField] private InputActionReference move;
         [SerializeField] private InputActionReference jump;
         [SerializeField] private float acceleration;
         [SerializeField] private float maxSpeed;
         [SerializeField] private float jumpForce;
+
+        [SerializeField, Tooltip("Force  to counter gravity for long jump"), Range(0, 9)]
+        private float counterGravityForce;
+
         private bool _isMoving;
+        private bool _isLongJumping;
         private bool _isGrounded;
         private Rigidbody2D _rb;
         private float _movement;
@@ -27,13 +36,22 @@ namespace Assets.Code.Player
                 move.action.canceled += Stop;
             }
 
-            if (jump)
-            {
-                jump.action.started += Jump;
-            }
+            if (!jump) return;
+
+            jump.action.started += Jump;
+            jump.action.performed += LongJump;
+            jump.action.canceled += CancelLongJump;
         }
 
-        private void Jump(InputAction.CallbackContext _) => _rb?.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        #region Custom Callbacks
+        private void CancelLongJump(InputAction.CallbackContext _) => _isLongJumping = false;
+
+        private void LongJump(InputAction.CallbackContext _)
+        {
+            _isLongJumping = true;
+            OnLongJumpStarted?.Invoke();
+        }
+
         private void Jump(InputAction.CallbackContext _)
         {
             if (!_isGrounded) return;
@@ -49,10 +67,21 @@ namespace Assets.Code.Player
             _movement = obj.ReadValue<float>();
         }
 
+        #endregion
+        
         private void FixedUpdate()
         {
-            if (!_rb || !_isMoving || MathF.Abs(_rb.linearVelocity.magnitude) > maxSpeed) return;
-            _rb.AddForce(new Vector2(_movement * acceleration * Time.fixedDeltaTime, 0));
+            if (!_rb) return;
+
+            if (_isMoving && MathF.Abs(_rb.linearVelocity.magnitude) < maxSpeed)
+            {
+                _rb.AddForce(new Vector2(_movement * acceleration * Time.fixedDeltaTime, 0), ForceMode2D.Force);
+            }
+
+            if (_isLongJumping)
+            {
+                _rb.AddForce(new Vector2(0, counterGravityForce * Time.fixedDeltaTime), ForceMode2D.Force);
+            }
         }
 
         private void OnDestroy()
@@ -63,10 +92,11 @@ namespace Assets.Code.Player
                 move.action.canceled -= Stop;
             }
 
-            if (jump)
-            {
-                jump.action.started -= Jump;
-            }
+            if (!jump) return;
+
+            jump.action.started -= Jump;
+            jump.action.performed -= LongJump;
+            jump.action.canceled -= CancelLongJump;
         }
 
         private void OnTriggerEnter(Collider other)
