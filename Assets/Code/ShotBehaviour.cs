219ba6921071e4a20428aff44071449cc5b18ced
LordMungi <lrdmungi@gmail.com>
2026-06-27 16:49:20 -0300

    Add Enemy

--- Assets/Code/ShotBehaviour.cs
+++ Assets/Code/ShotBehaviour.cs
@@ -3,18 +3,15 @@ using UnityEngine;
 public class ShotBehaviour : MonoBehaviour
 {
     private ShotSpawn _settings;
-    private Vector3 _movementDirection;
 
     public void Init(ShotSpawn shotSpawn)
     {
         _settings = shotSpawn;
         Destroy(gameObject, _settings.Lifetime);
-
-        _movementDirection = transform.rotation * _settings.Direction;
     }
 
     void Update()
     {
-        transform.Translate(_movementDirection * _settings.Speed * Time.deltaTime);
+        transform.Translate(_settings.Direction * _settings.Speed * Time.deltaTime);
     }
 }
