ebf750386c3abad065e6b57d02d0ee986c14c98e
LordMungi <lrdmungi@gmail.com>
2026-06-29 07:50:49 -0300

    Add first turrets

--- Assets/Code/Enemies/Shooter/ShotBehaviour.cs
+++ Assets/Code/Enemies/Shooter/ShotBehaviour.cs
@@ -4,9 +4,10 @@ public class ShotBehaviour : MonoBehaviour
 {
     private ShotSpawn _settings;
 
-    public void Init(ShotSpawn shotSpawn)
+    public void Init(ShotSpawn shotSpawn, GameObject parent)
     {
         _settings = shotSpawn;
+        gameObject.layer = parent.layer;
         Destroy(gameObject, _settings.Lifetime);
     }
 
